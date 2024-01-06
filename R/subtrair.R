e883f9fe529f04ef2a942c429d222fec9633f26e
Rodolpho Jordan Domingos Quintela <148010011+rodolphojordan@users.noreply.github.com>
2024-01-06 19:34:03 -0300

    first commit

---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#' Função para subtrair
+#'
+#' Esta funçõ serva para subtrair dois números
+#'
+#' @param a Um número
+#' @param b Outro número
+#'
+#' @examples
+#' subtrair(3,4)
+#'
+#' @export
+subtrair<- function(a,b){
+  a - b
+}
